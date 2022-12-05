43a84d77a4dd98529cb8c984e3558e8b924f2792
Breno Araujo <brenoaraujo50@icloud.com>
2022-12-05 13:18:44 +0000

    adiciona de forma correta o link

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+import { BsGithub, BsGlobe } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
@@ -10,21 +10,25 @@ function ProjectCards(props) {
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
+        <Card.Text style={{ textAlign: "justify", display: "flex", justifyContent: "space-around" }}>
           {props.description}
         </Card.Text>
         <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
-        <Button variant="primary" href={props.liveVersion} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.liveVersion ? "Blog" : "GitHub"}
+        {"\n"}
+        <Button
+          variant="primary"
+          href={props.liveVersion}
+          target="_blank"
+        >
+          <BsGlobe /> &nbsp;
+          {props.liveVersion ? "Live Version" : "GitHub"}
         </Button>
         {"\n"}
         {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
 
         {!props.isBlog && props.demoLink && (
           <Button
