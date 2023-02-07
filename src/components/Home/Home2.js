f8c8929419b272bcd1aaeced1af4bfba19e12976
Breno Araujo <brenoaraujo50@icloud.com>
2023-02-07 14:42:58 +0000

    retira a importação do instagram

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -5,7 +5,6 @@ import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
   AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
