40555c5f860301efe5b0c6e285998d703af589f1
Breno Araujo <brenoaraujo50@icloud.com>
2023-02-07 14:43:40 +0000

    remove o instagram do footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,16 +51,7 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/brenusaraujo/"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
+            
           </ul>
         </Col>
       </Row>
